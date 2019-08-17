76f32e5b04498e47e0bf3924e0b0a57e774cd6eb
dandy-dispatch-5270 <kylepcole@gmail.com>
2019-08-17 22:36:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   
   if (string == "I love you, Grandma.") {
     return "I love you, too."
-  } else if (string.toUpperCase == string) {
+  } else if (isUpperCase) {
     return "YES INDEED!";
   } else {
     return `I can't hear you!`;
