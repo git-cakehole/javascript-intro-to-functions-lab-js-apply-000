69e7e18b17f89f3e86db49ecbcfbc4d323427e9b
dandy-dispatch-5270 <kylepcole@gmail.com>
2019-08-17 22:34:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string == "I love you, Grandma.") {
+    return "I love you, too."
+  } else if (string.toUpperCase === string) {
+    return "YES INDEED!";
+  } else {
+    return `I can't hear you!`;
+  }
+}
